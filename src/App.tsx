61d190736b6647b311183420963b106e6a50d868
Baker3207 <awbaker99@gmail.com>
2024-03-06 14:33:15 -0700

    I set up base for React App Test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.<br></br>
+          Hello World!<br></br>
+          my wife is hot
         </p>
         <a
           className="App-link"
